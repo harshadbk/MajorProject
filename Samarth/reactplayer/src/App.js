c8e9d5b7cbf11873690310561b7475ca49956068
pagariasamarth@gmail.com <pagariasamarth@gmail.com>
2024-10-03 16:56:56 +0530

    added reactplayer

---
+++ Samarth/reactplayer/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ReactPlayer from "react-player";
+function App() {
+  return (
+    <div className="App">
+      <ReactPlayer
+        controls
+        url="https://youtu.be/se9DDAwwGQY?si=0H93BNbAFk3_VWHn"
+      />
+    </div>
+  );
+}
+
+export default App;
